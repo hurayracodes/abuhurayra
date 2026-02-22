42c300eb7ac483107cf45a31eb42136df145b310
Abu Hurayra <abuhuraira3912l@gmail.com>
2026-02-22 14:39:35 +0500

    cnavas setting

--- src/ui/StarsCanvas.jsx
+++ src/ui/StarsCanvas.jsx
@@ -4,7 +4,6 @@ import { Points, PointMaterial } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import * as random from "maath/random";
 import { useState, useRef, Suspense } from "react";
-import { Points as PointsType } from "three";
 
 export const StarBackground = (props) => {
   const ref = useRef(null);
